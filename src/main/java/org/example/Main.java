a5b26337f04c01cb81c895102bf78b39798bfd3a
onuraden <gurcayonur@outlook.com>
2025-05-06 00:10:42 +0300

    completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,61 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(area(5.0, 4.0));
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if(isBarking) {
+            if (clock < 0 || clock > 23) return false;
+            return switch (clock) {
+                case 0, 1, 2, 3, 4, 5, 6, 7, 20, 21, 22, 23 -> true;
+                default -> false;
+            };
+        } else
+            return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if(firstAge >= 13 && firstAge <= 19) {
+            return true;
+        } else if(secondAge >= 13 && secondAge <= 19) {
+            return true;
+        } else if(thirdAge >= 13 && thirdAge <= 19) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if(isSummer) {
+            if(temp >= 25 && temp <=45) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if(temp >= 25 && temp <=35) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
     public static double area(double width, double height) {
-       
+        if(width > 0 && height > 0) {
+            return width*height;
+        } else {
+            return -1;
+        }
     }
 
     public static double area(double radius) {
-       
+        if(radius > 0) {
+            return radius*radius*Math.PI;
+        } else {
+            return -1;
+        }
     }
 }
